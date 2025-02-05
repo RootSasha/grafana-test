b6589e28190651c8126b577ac23280a83bae915c
Маньковський Олександр <117378642+RootSasha@users.noreply.github.com>
2025-02-05 11:58:28 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 apt update
 apt install docker.io -y
-docker swarm init
-docker stack deploy -c docker-compose.yml monitoring
+snap install docker -y
+#docker swarm init
+#docker stack deploy -c docker-compose.yml monitoring
