3c8cd9c3fe0fb0f250f70cdc73682fe0693e52fc
Маньковський Олександр <117378642+RootSasha@users.noreply.github.com>
2025-02-19 19:19:08 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-apt update
-apt install docker-compose -y
-docker-compose up -d
+sudo apt update
+sudo apt install docker-compose -y
+sudo docker-compose up -d
 #docker swarm init
 #docker stack deploy -c docker-compose.yml monitoring
 touch hello.txt
